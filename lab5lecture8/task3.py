d07a473a9eadca96cae3f7ad3f742738c420b01d
Ivhen <Ivhen@dsafs.sdaf>
2018-11-25 18:15:59 +0300

    lab 5 task 3 done

---
+++ lab5lecture8/task3.py
@@ -0,0 +1,22 @@
+"""
+recursive koch curve
+"""
+
+import turtle as T
+
+def draw_koch(l, n):
+    if n == 0:
+        T.forward(l)
+        return
+    draw_koch(l / 3, n - 1)
+    T.left(60)
+    draw_koch(l / 3, n - 1)
+    T.right(120)
+    draw_koch(l / 3, n - 1)
+    T.left(60)
+    draw_koch(l / 3, n - 1)
+
+
+
+T.speed(0)
+draw_koch(300, 5)
